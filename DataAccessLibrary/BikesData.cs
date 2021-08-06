161dd5ede0d1e35b8fd770fb6018065354fa67bc
Lukasz Kandziora <lukasz.kandziora@ingenico.com>
2021-08-06 14:06:02 +0200

    bikes view improvements

--- DataAccessLibrary/BikesData.cs
+++ DataAccessLibrary/BikesData.cs
@@ -15,13 +15,8 @@ namespace DataAccessLibrary
 
         public Task<List<BikeModel>> GetBikes()
         {
-            string sql = @"select b.""Id"", ""Price"", ""Model"", ""Make"" from public.""Bikes"" b left join public.""Products"" p on b.""Id"" = p.""Id""";
-            var x = _db.LoadData<BikeModel, dynamic>(sql, new { });
-
-            //var x =_db.LoadData<>
-            //return _db.LoadData<BikeModel, dynamic>(sql, new { });
-
-            return x;
+            string sql = "SELECT * FROM public.\"BikesView\"";
+            return _db.LoadData<BikeModel, dynamic>(sql, new { });
         }
     }
 }
